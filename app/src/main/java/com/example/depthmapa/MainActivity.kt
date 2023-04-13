8914c4f488df24eba3244d7eca033f21770f8a57
roska <r.oskar.15@wp.pl>
2023-04-13 15:07:05 +0200

    Initial commit + Adding button which leads you to picking img from your gallery

---
+++ app/src/main/java/com/example/depthmapa/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.depthmapa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button:Button
+    private lateinit var imageView: ImageView
+    companion object{
+        val IMAGE_REQUSET_CODE = 100
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btn_pick_img)
+        imageView = findViewById(R.id.img_save)
+
+        button.setOnClickListener{
+            pickImageGallery()
+        }
+    }
+    private fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUSET_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUSET_CODE && resultCode == RESULT_OK){
+            imageView.setImageURI(data?.data)
+        }
+    }
+}
